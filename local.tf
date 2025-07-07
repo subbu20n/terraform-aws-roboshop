b6b686b5279a9d324e7d3ceb04ad9fee4dd017ce
subbu <subbunandyala48@gmail.com>
2025-07-07 19:16:59 +0530

    terraform

--- local.tf
+++ local.tf
@@ -1,8 +1,8 @@
 locals {
 
     vpc_id = data.aws_ssm_parameter.vpc_id.value 
-    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value 
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value 
