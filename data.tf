d713c74ff033179a916607240e136dc4af5944a1
subbu <subbunandyala48@gmail.com>
2026-01-12 21:48:17 +0530

    terraform

--- data.tf
+++ data.tf
@@ -21,7 +21,7 @@ data "aws_ssm_parameter" "frontend_alb_listener_arn" {
 data "aws_ami" "joindevops" {
   owners           = ["973714476881"]
   most_recent      = true
-
+ 
   filter {
     name   = "name"  
     values = ["RHEL-9-DevOps-Practice"]
