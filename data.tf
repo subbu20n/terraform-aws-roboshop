b7437bb1c5df436237f98dd8d10efe5b91dff76b
subbu <subbunandyala48@gmail.com>
2026-04-08 08:54:25 +0530

    roboshop

--- data.tf
+++ data.tf
@@ -16,7 +16,7 @@ data "aws_ssm_parameter" "backend_alb_listener_arn" {
 
 data "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
-}
+} 
 
 data "aws_ami" "joindevops" {
   owners           = ["973714476881"]
