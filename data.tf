4cd1cde84a5abaf738c4be8c8c397b39a180af55
subbu <subbunandyala48@gmail.com>
2026-04-15 22:43:50 +0530

    terraform

--- data.tf
+++ data.tf
@@ -12,7 +12,7 @@ data "aws_ssm_parameter" "sg_id" {
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
   name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
-}
+}  
 
 data "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
